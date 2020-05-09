84487044d211863e62d1ea99d3ba8df2bcfda5ca
Mary Mohlenkamp <60491262+marymkamp@users.noreply.github.com>
2020-05-08 21:55:26 -0400

    git commit

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+function ex() {
+
+var x = document.createElement ("IMG")
+x.setAttribute("display_image","center")
+x.setAttribute("src", "headshot.jpg");
+x.setAttribute("width", "25%");
+x.setAttribute("height", "25%");
+x.setAttribute("alt", "Kim_Picture");
+document.body.appendChild(x);
+
+}
+
